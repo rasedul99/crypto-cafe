b946457abf6b6cfa9e1ce163911ea735dbe435e0
rasedul99 <rasedul2199@gmail.com>
2022-04-09 02:47:34 +0600

    Spinner

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -4,9 +4,10 @@ import Spinner from "../Spinner/Spinner";
 
 const Coins = () => {
   const [coins, SetCoins] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
     )
@@ -19,15 +20,16 @@ const Coins = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="px-4 pb-24  mx-auto max-w-7xl md:px-2">
-          <p className="text-center text-3xl font-bold text-gray">
+        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+          <p className='text-center text-3xl font-bold text-gray-800'>
             Available Crypto Currencies
           </p>
-          <p className="text-center mb-12 text-xl font-normal text-gray-500 ">
-            Total Coins :{coins.length}
+          <p className='text-center mb-12 text-xl font-normal text-gray-500 '>
+            Total coins: {coins.length}
           </p>
-          <div className=" grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-            {coins.map((coin) => (
+
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center'>
+            {coins.map(coin => (
               <CoinCard key={coin.id} coin={coin} />
             ))}
           </div>
